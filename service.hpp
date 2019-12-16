76fd71c9d9e5550b5394f5bb6491c86f0b334888
Chris <chrisfontas724@gmail.com>
2019-12-15 17:26:08 -0800

    update header comments

--- service.hpp
+++ service.hpp
@@ -1,5 +1,5 @@
-// Copyright 2019 Sic Studios. All rights reserved.
-// Use of this source code is governed by our license that can be
+// Copyright 2019 Chris Fontas. All rights reserved.
+// Use of this source code is governed by the license that can be
 // found in the LICENSE file.
 
 #ifndef RTTI_SERVICE_HPP_
