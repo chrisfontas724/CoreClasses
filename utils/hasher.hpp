18a9602ab3874b33282e8cc737e0587eeccf3076
Chris Fontas <chrisfontas724@gmail.com>
2020-06-21 12:58:30 -0700

    fix formatting

--- utils/hasher.hpp
+++ utils/hasher.hpp
@@ -9,23 +9,21 @@
 
 namespace cxl {
 class Hasher {
-public:
-        
-        uint32_t hashString(const std::string& str);
-        
-        uint32_t hashArray(uint32_t* arr, uint32_t num);
-        
-        template<typename T>
-        uint32_t hash(T item, uint32_t seed) {
-            return crc32(static_cast<uint8_t*>(&item), sizeof(T), seed);
-        }
+   public:
+    uint32_t hashString(const std::string& str);
+
+    uint32_t hashArray(uint32_t* arr, uint32_t num);
+
+    template <typename T>
+    uint32_t hash(T item, uint32_t seed) {
+        return crc32(static_cast<uint8_t*>(&item), sizeof(T), seed);
+    }
 
     static uint32_t simpleHash(const std::string& str);
-private:
-        
+
+   private:
     uint32_t crc32(const uint8_t* data, uint32_t size, uint32_t crc = 0);
-
 };
-} // cxl
+}  // namespace cxl
 
-#endif // CORE_UTILS_HASHER_HPP_
+#endif  // CORE_UTILS_HASHER_HPP_
--- utils/string_utils.hpp
+++ utils/string_utils.hpp
@@ -5,7 +5,7 @@
 #ifndef STRING_UTILS_HPP_
 #define STRING_UTILS_HPP_
 
-#include <string> 
+#include <string>
 
 namespace cxl {
 namespace utils {
@@ -13,45 +13,42 @@ namespace utils {
 inline uint64_t simpleHash(const std::string& str) {
     // fnv-1a
     uint64_t hash = 2166136261UL;
-    if (str.c_str()){
+    if (str.c_str()) {
         const char* c = str.c_str();
-        while(*c != '\0') {
+        while (*c != '\0') {
             hash = (hash ^ (*c++)) * 16777619;
         }
     }
     return hash;
 }
-        
-inline uint64_t hash(const std::string& str) {
-    return simpleHash(str);
-}
- 
+
+inline uint64_t hash(const std::string& str) { return simpleHash(str); }
+
 inline std::string substringLastOccurence(const std::string& s, const char c) {
     std::string result = s;
     auto pos = result.rfind(c);
     if (pos != std::string::npos) {
-        result.erase(0,pos+1);
+        result.erase(0, pos + 1);
     }
     return result;
 }
-        
+
 inline bool containsSubstring(const std::string& s1, const std::string& s2) {
     return s1.find(s2) != std::string::npos;
 }
-        
-inline std::string stringBetweenDeliminators(const std::string& string, 
-                                             const std::string& first,
+
+inline std::string stringBetweenDeliminators(const std::string& string, const std::string& first,
                                              const std::string& last) {
     uint64_t first_lim = string.find(first);
-    uint64_t last_lim =  string.rfind(last);
+    uint64_t last_lim = string.rfind(last);
     if (first_lim != std::string::npos && last_lim != std::string::npos) {
-        std::string str_new = string.substr(first_lim, last_lim - first_lim);     
-        str_new = str_new.substr(first.size());       
+        std::string str_new = string.substr(first_lim, last_lim - first_lim);
+        str_new = str_new.substr(first.size());
         return str_new;
     }
     return "";
 }
-        
+
 inline bool replace(std::string& str, const std::string& from, const std::string& to) {
     uint64_t start_pos = str.find(from);
     if (start_pos == std::string::npos) {
@@ -60,19 +57,19 @@ inline bool replace(std::string& str, const std::string& from, const std::string
     str.replace(start_pos, from.length(), to);
     return true;
 }
-        
+
 inline bool replaceAll(std::string& str, const std::string& from, const std::string& to) {
     const std::string s = "*A";
     const std::string t = "*A\n";
     std::string::size_type n = 0;
-    while ( ( n = str.find( from, n ) ) != std::string::npos ) {
+    while ((n = str.find(from, n)) != std::string::npos) {
         str.replace(n, from.size(), to);
         n += to.size();
     }
     return true;
 }
 
-} // utils
-} // cxl
+}  // namespace utils
+}  // namespace cxl
 
-#endif // STRING_UTILS_HPP_
+#endif  // STRING_UTILS_HPP_
